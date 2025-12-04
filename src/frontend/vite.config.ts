48b33d35579ad9525f7aca59dfd376970247cadb
Dean Martin <dean.martin@frostaura.net>
2025-12-05 00:41:31 +0200

    fix(infra): prod hostname support

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -1,43 +1,44 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@store': path.resolve(__dirname, './src/store'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@pages': path.resolve(__dirname, './src/pages'),
-      '@hooks': path.resolve(__dirname, './src/hooks'),
-      '@services': path.resolve(__dirname, './src/services'),
-      '@utils': path.resolve(__dirname, './src/utils'),
-      '@types': path.resolve(__dirname, './src/types'),
-    },
-  },
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          redux: ['@reduxjs/toolkit', 'react-redux'],
-          charts: ['recharts'],
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+            '@store': path.resolve(__dirname, './src/store'),
+            '@components': path.resolve(__dirname, './src/components'),
+            '@pages': path.resolve(__dirname, './src/pages'),
+            '@hooks': path.resolve(__dirname, './src/hooks'),
+            '@services': path.resolve(__dirname, './src/services'),
+            '@utils': path.resolve(__dirname, './src/utils'),
+            '@types': path.resolve(__dirname, './src/types'),
         },
-      },
     },
-    minify: 'terser',
-    sourcemap: true,
-  },
-  server: {
-    port: 5173,
-    host: '0.0.0.0',
-    proxy: {
-      '/api': {
-        target: 'http://backend:5000',
-        changeOrigin: true,
-      },
+    build: {
+        outDir: 'dist',
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    vendor: ['react', 'react-dom', 'react-router-dom'],
+                    redux: ['@reduxjs/toolkit', 'react-redux'],
+                    charts: ['recharts'],
+                },
+            },
+        },
+        minify: 'terser',
+        sourcemap: true,
     },
-  },
+    server: {
+        port: 5173,
+        host: '0.0.0.0',
+        allowedHosts: ['.localhost', '.localdomain', 'lifeos.frostaura.net'],
+        proxy: {
+            '/api': {
+                target: 'http://backend:5000',
+                changeOrigin: true,
+            },
+        },
+    },
 })
